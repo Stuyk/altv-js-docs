08e88ab2ff5a05b46d1175635cca94f2ca32d5cf
Stuyk <Stuyk@users.noreply.github.com>
2022-09-08 17:29:52 +0000

    deploy: 30908cc7fcefbf09092c81631803890a70f9833c

---
+++ assets/enums_modelType.md.ea23515f.js
@@ -0,0 +1,21 @@
+import{_ as a,o as e,c as l,b as s,d as o,t as p,e as t}from"./app.66773060.js";const _=JSON.parse('{"title":"ModelType","description":"","frontmatter":{"title":"ModelType","order":0},"headers":[{"level":2,"title":"Enum Definition","slug":"enum-definition","link":"#enum-definition","children":[]},{"level":2,"title":"Documentation","slug":"documentation","link":"#documentation","children":[]}],"relativePath":"enums/modelType.md"}'),c={name:"enums/modelType.md"},i={id:"frontmatter-title",tabindex:"-1"},r=s("a",{class:"header-anchor",href:"#frontmatter-title","aria-hidden":"true"},"#",-1),D=t(`<h2 id="enum-definition" tabindex="-1">Enum Definition <a class="header-anchor" href="#enum-definition" aria-hidden="true">#</a></h2><div class="language-ts"><button class="copy"></button><span class="lang">ts</span><pre><code><span class="line"><span style="color:#C792EA;">enum</span><span style="color:#A6ACCD;"> </span><span style="color:#FFCB6B;">ModelType</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Invalid</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Ped</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Automobile</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Plane</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Trailer</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    QuadBike</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    SubmarineCar</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    AmphibiousAutomobile</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    AmphibiousQuadBike</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Heli</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Blimp</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Autogyro</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Bike</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Bmx</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Boat</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Train</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Submarine</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#A6ACCD;">    Object</span><span style="color:#89DDFF;">,</span></span>
+<span class="line"><span style="color:#89DDFF;">};</span></span>
+<span class="line"></span></code></pre></div><h2 id="documentation" tabindex="-1">Documentation <a class="header-anchor" href="#documentation" aria-hidden="true">#</a></h2>`,3);function d(n,y,A,C,F,m){return e(),l("div",null,[s("h1",i,[o(p(n.$frontmatter.title)+" ",1),r]),D])}const h=a(c,[["render",d]]);export{_ as __pageData,h as default};
