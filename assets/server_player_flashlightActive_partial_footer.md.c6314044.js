5f640173f056c3ef5cb2004d56238c4d71ff2d68
Stuyk <Stuyk@users.noreply.github.com>
2022-09-09 20:57:07 +0000

    deploy: d92ca07c697c05285866f15679a878a77fe1f59c

--- assets/server_player_flashlightActive_partial_footer.md.c6314044.js
+++
@@ -1,9 +0,0 @@
-import{_ as s,o as a,c as n,e}from"./app.80c976a6.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[{"level":3,"title":"Examples","slug":"examples","link":"#examples","children":[]}],"relativePath":"server/player/flashlightActive_partial_footer.md"}'),l={name:"server/player/flashlightActive_partial_footer.md"},p=e(`<h3 id="examples" tabindex="-1">Examples <a class="header-anchor" href="#examples" aria-hidden="true">#</a></h3><div class="language-ts"><button class="copy"></button><span class="lang">ts</span><pre><code><span class="line"><span style="color:#C792EA;">const</span><span style="color:#A6ACCD;"> isActive </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> player</span><span style="color:#89DDFF;">.</span><span style="color:#A6ACCD;">flashlightActive</span><span style="color:#89DDFF;">;</span></span>
-<span class="line"></span>
-<span class="line"><span style="color:#89DDFF;">if</span><span style="color:#A6ACCD;"> (</span><span style="color:#89DDFF;">!</span><span style="color:#A6ACCD;">isActive) </span><span style="color:#89DDFF;">{</span></span>
-<span class="line"><span style="color:#89DDFF;">    </span><span style="color:#676E95;">// I can&#39;t see anything captain!</span></span>
-<span class="line"><span style="color:#F07178;">    </span><span style="color:#89DDFF;">return</span><span style="color:#89DDFF;">;</span></span>
-<span class="line"><span style="color:#89DDFF;">}</span></span>
-<span class="line"></span>
-<span class="line"><span style="color:#676E95;">// I can see everything captain!</span></span>
-<span class="line"></span></code></pre></div>`,2),t=[p];function o(c,r,i,_,D,y){return a(),n("div",null,t)}const F=s(l,[["render",o]]);export{h as __pageData,F as default};
