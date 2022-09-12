09d8132f227ce22cf46b48cca0527b7a8e0b7add
Stuyk <Stuyk@users.noreply.github.com>
2022-09-12 12:59:53 +0000

    deploy: f157b57e0d7abdc63ea48f3602d0e9296e81aca0

---
+++ assets/server_player_currentWeapon_partial_footer.md.4a427da0.js
@@ -0,0 +1,9 @@
+import{_ as s,o as a,c as n,e}from"./app.e1ffc971.js";const F=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[{"level":3,"title":"Examples","slug":"examples","link":"#examples","children":[]}],"relativePath":"server/player/currentWeapon_partial_footer.md"}'),l={name:"server/player/currentWeapon_partial_footer.md"},p=e(`<h3 id="examples" tabindex="-1">Examples <a class="header-anchor" href="#examples" aria-hidden="true">#</a></h3><div class="language-ts"><button class="copy"></button><span class="lang">ts</span><pre><code><span class="line"><span style="color:#C792EA;">const</span><span style="color:#A6ACCD;"> currentWeapon </span><span style="color:#89DDFF;">=</span><span style="color:#A6ACCD;"> player</span><span style="color:#89DDFF;">.</span><span style="color:#A6ACCD;">currentWeapon</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#89DDFF;">if</span><span style="color:#A6ACCD;"> (currentWeapon </span><span style="color:#89DDFF;">!==</span><span style="color:#A6ACCD;"> </span><span style="color:#F78C6C;">0x958a4a8f</span><span style="color:#A6ACCD;">) </span><span style="color:#89DDFF;">{</span></span>
+<span class="line"><span style="color:#89DDFF;">    </span><span style="color:#676E95;">// is not using a baseball bat</span></span>
+<span class="line"><span style="color:#F07178;">    </span><span style="color:#89DDFF;">return</span><span style="color:#89DDFF;">;</span></span>
+<span class="line"><span style="color:#89DDFF;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;">// is using a baseball bat</span></span>
+<span class="line"></span></code></pre></div>`,2),o=[p];function t(r,c,i,_,D,y){return a(),n("div",null,o)}const u=s(l,[["render",t]]);export{F as __pageData,u as default};
